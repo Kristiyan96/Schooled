de150056b7b7034040ce50b7074c8648816adc01
Nikola Jichev <njichev@gmail.com>
2018-05-17 15:37:42 +0300

    Validate email.

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -10,6 +10,9 @@ module Authentication
     @authenticatable << base
     base.class_eval do
       has_secure_password
+      validates :email, presence: true,
+                        uniqueness: { case_sensitive: false },
+                        email: true
     end
   end
 end
