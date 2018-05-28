fc8d2fa874070854726d267ecd05338d050032b3
Kristiyan <kristiyan.c@gmail.com>
2018-05-28 10:55:26 +0300

    Authentication (#46)
    * Working registration
    
    * Forgot password form
    
    * Sending confirmation email

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   resources :schools
 
   root "pages#show", page: "home"
