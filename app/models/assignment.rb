2d20b80dd0bbdbdaf38950474fa817d17be64dc3
Nikola Jichev <njichev@gmail.com>
2018-05-28 20:47:27 +0300

    Create headmaster invitations.

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -2,4 +2,9 @@ class Assignment < ApplicationRecord
   belongs_to :role
   belongs_to :user
   belongs_to :school
+
+  def self.invite(school:, role:, user:)
+    user = User.find_by(email: user[:email]) || User.invite!(user)
+    create(school: school, role: role, user: user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
-  resources :schools
+  resources :schools do
+    resources :headmaster_invitations, path: :headmasters, module: :schools, only: [:index, :create]
+  end
 
   root "pages#show", page: "home"
-	get '/pages/*page', to: 'pages#show'
+  get '/pages/*page', to: 'pages#show'
 end
