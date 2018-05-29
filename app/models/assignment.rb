1f177231e8f59977b4f30efeafdcc2dd82770a2a
Kristiyan <kristiyan.c@gmail.com>
2018-05-29 07:43:58 +0300

    Role specific views (#75)
    * Fix buggy dropdown
    
    * Add login/logout to navbar
    
    * Custom sidebar for different roles
    
    * Most important buttons in sidebar and navbar

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -3,12 +3,11 @@ class Assignment < ApplicationRecord
   belongs_to :user
   belongs_to :school
 
-
   def self.invite(school:, role:, user:)
     user = User.find_by(email: user[:email]) || User.invite!(user)
     create(school: school, role: role, user: user)
   end
-
+  
   def name
     "#{role.name} at #{school.name}"
   end
