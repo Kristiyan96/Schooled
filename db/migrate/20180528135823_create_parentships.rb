902335fcb2cbbe0bdc56ded66f80d1dc4068cd6a
Nikola Jichev <njichev@gmail.com>
2018-05-30 16:25:34 +0300

    Parent is parent only once. (#87)

--- db/migrate/20180528135823_create_parentships.rb
+++ db/migrate/20180528135823_create_parentships.rb
@@ -3,8 +3,10 @@ class CreateParentships < ActiveRecord::Migration[5.2]
     create_table :parentships do |t|
       t.integer :parent_id
       t.integer :student_id
-      
+
       t.timestamps
     end
+
+    add_index :parentships, [:parent_id, :student_id], unique: true
   end
 end
