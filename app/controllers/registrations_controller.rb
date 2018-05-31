4dc9c75ef257a82c2b60f4aa6f700ef7795d1e22
kristiyan <kristiyan.c@gmail.com>
2018-05-31 19:38:39 +0300

    Allow user to edit profile without current password

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,6 +7,14 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def account_update_params
-    params.require(:user).permit(:first_name, :middle_name, :address, :phone_number, :birthday, :last_name, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:first_name, :middle_name, :last_name, :address, :phone_number, :birthday, :password, :password_confirmation, :current_password)
+  end
+
+  def update_resource(resource, params)
+    if params[:current_password].blank?
+     resource.update_without_password(params.except(:current_password))
+    else
+      resource.update_with_password(params)
+    end
   end
 end
