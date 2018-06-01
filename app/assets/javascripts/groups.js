05bc3e079320133a1ab3df042cfdef0322cb8646
kristiyan <kristiyan.c@gmail.com>
2018-06-01 03:37:51 +0300

    Adding parent using remote form

---
+++ app/assets/javascripts/groups.js
@@ -0,0 +1,11 @@
+document.addEventListener("turbolinks:load", function() {
+  $('.td-actions .add-parent').on('click', function(){
+    $(this).parent().parent().next('tr').toggleClass('d-none');
+  })
+});
+
+document.addEventListener("turbolinks:load", function() {
+  $('.td-actions .invite-parent').on('click', function(){
+    $(this).parent().parent().toggleClass('d-none');
+  })
+});
--- app/controllers/students/parentship_controller.rb
+++ app/controllers/students/parentship_controller.rb
@@ -1,12 +1,17 @@
 class Students::ParentshipController < ApplicationController
   def create
-    student = User.find(params[:student_id])
-    is_invited = Parentship.invite_parent(student: student, parent: user_params)
+    @student = User.find(params[:student_id])
+    @is_invited = Parentship.invite_parent(student: @student, parent: user_params)
 
-    if is_invited
-      redirect_back fallback_location: profile_path(student), notice: "Invited!"
-    else
-      redirect_back fallback_location: profile_path(student), alert: "Failed to invite!"
+    respond_to do |format|
+      if @is_invited
+        @user = User.find_by(email: user_params[:email])
+        format.html { redirect_back fallback_location: profile_path(@student), notice: "Invited!" }
+        format.js { render 'schools/parent_invitations/create' }
+      else
+        format.html { redirect_back fallback_location: profile_path(@student), alert: "Failed to invite!" }
+        format.js { render 'schools/parent_invitations/create' }
+      end
     end
   end
 
