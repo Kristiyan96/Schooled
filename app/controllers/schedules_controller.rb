e70ea6f222138bdfdd4e1c497901680cecc28a99
Nikola Jichev <njichev@gmail.com>
2018-06-07 00:48:53 +0300

    Add schedule controllers. (#104)

---
+++ app/controllers/schedules_controller.rb
@@ -0,0 +1,46 @@
+class SchedulesController < ApplicationController
+  def index
+    @school = School.find(params[:school_id])
+
+    @schedules = Schedule.where(course: @school.courses)
+  end
+
+  def create
+    school = School.find(params[:school_id])
+
+    # NOTE(for Kris) Type param should be one of [:one, :series_7, :series_14]
+    Schedule.create_with_type(school: school, **schedule_params)
+
+    redirect_to root_path
+  end
+
+  def update
+    school = School.find(params[:id])
+    schedule = Schedule.find(params[:id])
+
+    # Type param should be one of [:one, :series_7, :series_14]
+    Schedule.update_with_type(school: school, schedule: schedule, update_params)
+
+    redirect_to root_path
+  end
+
+  def destroy
+    school = School.find(params[:id])
+    schedule = Schedule.find(params[:id])
+
+    # Type param should be one of [:one, :series_7, :series_14]
+    Schedule.destroy_with_type(school: school, schedule: schedule, type: params[:schedules][:type])
+
+    redirect_to root_path
+  end
+
+  private
+
+  def schedule_params
+    params.require(:schedules).permit(:course_id, :time_slot_id, :type)
+  end
+
+  def update_params
+    params.require(:schedules).permit(:course_id, :type)
+  end
+end
