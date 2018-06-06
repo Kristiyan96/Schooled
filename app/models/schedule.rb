7cbc818643733565da094376ef6424026d2bfeb2
Kristiyan <kristiyan.c@gmail.com>
2018-06-06 16:23:38 +0300

    Update schedule.rb

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,7 +2,7 @@ class Schedule < ApplicationRecord
   belongs_to :time_slot
   belongs_to :course
 
-  scope :with_courses_for_period, -> group, start, finish {
+  scope :courses_in_period, -> group, start, finish {
     joins(:course).joins(:time_slot)
       .where(courses: { group: group })
       .where('time_slots.start > ?', start)
