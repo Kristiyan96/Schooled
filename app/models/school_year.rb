d0a8e76da486ae9110b5118923addbc046348f8f
Nikola Jichev <njichev@gmail.com>
2018-06-07 13:41:37 +0300

    Add unique active (#108)
    * Unique active school year on school.

--- app/models/school_year.rb
+++ app/models/school_year.rb
@@ -1,4 +1,12 @@
 class SchoolYear < ApplicationRecord
   belongs_to :school
   has_many :courses
+
+  validates_uniqueness_of :active, if: :active, scope: :school_id
+
+  def self.create_active(params)
+    year = new(params)
+    where(school: year.school).update_all(active: false)
+    year.save
+  end
 end
