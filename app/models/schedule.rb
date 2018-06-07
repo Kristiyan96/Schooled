d9649348fe15daa99fb2b2ec48bd8007c38ed927
Nikola Jichev <njichev@gmail.com>
2018-06-07 20:17:35 +0300

    Fix interval (#110)
    Fix them by comparing hour + minutes.

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -12,8 +12,12 @@ class Schedule < ApplicationRecord
   # Interval is in days
   scope :with_interval_from_start_and_end, -> start, finish, interval {
     joins(:time_slot)
-      .where("CAST(DATE_PART('day', time_slots.start - ?) as INTEGER) % ? = 0", start, interval)
-      .where("CAST(DATE_PART('day', time_slots.end - ?) as INTEGER) % ? = 0", finish, interval)
+      .where("DATE_PART('hour', time_slots.start) = ?", start.hour)
+      .where("DATE_PART('minute', time_slots.start) = ?", start.min)
+      .where("DATE_PART('hour', time_slots.end) = ?", finish.hour)
+      .where("DATE_PART('minute', time_slots.end) = ?", finish.min)
+      .where("CAST(DATE_PART('day', time_slots.start - ?) AS INTEGER) % ? = 0", start, interval)
+  }
   }
   scope :with_start_date_greater_than, -> date {
     joins(:time_slot)
@@ -48,11 +52,11 @@ class Schedule < ApplicationRecord
     when :one
       schedule.update(course_id: course_id)
     when :series_7
-      Schedule.with_interval_from_start_and_end(time_slot.start, time_slot.end, 7.days)
+      Schedule.with_interval_from_start_and_end(time_slot.start, time_slot.end, 7)
         .with_start_date_greater_than(time_slot.start)
         .update_all(course_id: course_id)
     when :series_14
-      Schedule.with_interval_from_start_and_end(time_slot.start, time_slot.end, 14.days)
+      Schedule.with_interval_from_start_and_end(time_slot.start, time_slot.end, 14)
         .with_start_date_greater_than(time_slot.start)
         .update_all(course_id: course_id)
     end
@@ -65,11 +69,11 @@ class Schedule < ApplicationRecord
     when :one
       schedule.destroy
     when :series_7
-      Schedule.with_interval_from_start_and_end(time_slot.start, time_slot.end, 7.days)
+      Schedule.with_interval_from_start_and_end(time_slot.start, time_slot.end, 7)
         .with_start_date_greater_than(time_slot.start)
         .destroy_all
     when :series_14
-      Schedule.with_interval_from_start_and_end(time_slot.start, time_slot.end, 14.days)
+      Schedule.with_interval_from_start_and_end(time_slot.start, time_slot.end, 14)
         .with_start_date_greater_than(time_slot.start)
         .destroy_all
     end
