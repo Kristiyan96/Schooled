9df4e0bfdc9c46a707c1abb0800e912ab38d3334
Nikola Jichev <njichev@gmail.com>
2018-06-07 13:40:36 +0300

    Unique active school year on school.

--- app/models/school_year.rb
+++ app/models/school_year.rb
@@ -3,4 +3,10 @@ class SchoolYear < ApplicationRecord
   has_many :courses
 
   validates_uniqueness_of :active, if: :active, scope: :school_id
+
+  def self.create_active(params)
+    year = new(params)
+    where(school: year.school).update_all(active: false)
+    year.save
+  end
 end
