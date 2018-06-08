94872a23e92fe8aabd0fe84a6c32962823ae2fdf
Kristiyan <kristiyan.c@gmail.com>
2018-06-08 15:33:47 +0300

    Update absences_controller.rb

--- app/controllers/absences_controller.rb
+++ app/controllers/absences_controller.rb
@@ -55,7 +55,7 @@ class AbsencesController < ApplicationController
   end
 
   def absence_params
-    params.require(:absence).permit(:student_id, :kind, :category, :school_year_id, :course_id, :schedule_id)
+    params.require(:absence).permit(:student_id, :kind, :category, :schedule_id)
   end
 
   def value
