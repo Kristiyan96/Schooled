0cd99c32dc9a218100523b2e97692391ea1a0c91
Nikola Jichev <njichev@gmail.com>
2018-06-08 15:37:45 +0300

    Only one final/semester mark. (#114)

--- app/models/mark.rb
+++ app/models/mark.rb
@@ -3,4 +3,7 @@ class Mark < ApplicationRecord
   belongs_to :student, foreign_key: "student_id", class_name: "User"
 
   enum kind: [:first_semester, :first_semester_final, :second_semester, :second_semester_final, :final]
+  validates_uniqueness_of :kind, if: :first_semester_final?, scope: [:student_id, :course]
+  validates_uniqueness_of :kind, if: :second_semester_final?, scope: [:student_id, :course]
+  validates_uniqueness_of :kind, if: :final?, scope: [:student_id, :course]
 end
