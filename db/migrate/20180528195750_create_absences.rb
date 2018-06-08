5eb65d1e3ea4a01c65f27568f07a5498d7bdcbb8
Kristiyan <kristiyan.c@gmail.com>
2018-06-08 15:32:44 +0300

    Update 20180528195750_create_absences.rb

--- db/migrate/20180528195750_create_absences.rb
+++ db/migrate/20180528195750_create_absences.rb
@@ -6,7 +6,7 @@ class CreateAbsences < ActiveRecord::Migration[5.2]
       t.integer :category, default: 0, null: false
       t.integer :student_id, index: true
 
-      t.references :course, foreign_key: true, index: true
+      t.references :schedule, foreign_key: true, index: true
       t.references :school_year, foreign_key: true, index: true
 
       t.timestamps
