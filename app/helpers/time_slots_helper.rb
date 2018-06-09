64bc6948003d19af102eff411b3883fdab6fb4e2
kristiyan <kristiyan.c@gmail.com>
2018-06-09 18:26:12 +0300

    Showing week schedule

--- app/helpers/time_slots_helper.rb
+++ app/helpers/time_slots_helper.rb
@@ -1,5 +1,5 @@
 module TimeSlotsHelper
   def slot_period(slot)
-    "#{slot.start.strftime('%H:%M')} - #{slot.end.strftime('%H:%M')}"
+    "#{slot.start.strftime('%H:%M')} - #{slot.end.strftime('%H:%M')}" if slot
   end
 end
