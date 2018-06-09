39e380a8d06a6447e2edc32452899afe94aae61e
kristiyan <kristiyan.c@gmail.com>
2018-06-09 22:20:38 +0300

    Creating time slots from today onwards

--- app/controllers/time_slots_controller.rb
+++ app/controllers/time_slots_controller.rb
@@ -12,10 +12,10 @@ class TimeSlotsController < ApplicationController
 
   def create
     @school     = School.find(params[:school_id])
-    @date       = Date.parse(params[:date])
+    @date       = (params[:date] && Date.parse(params[:date])) || Date.today
     school_year = SchoolYear.find(time_slot_params[:school_year_id])
 
-    TimeSlot.create_week_daily(school_year: school_year, params: time_slot_params)
+    TimeSlot.create_week_daily(school_year: school_year, date: @date, params: time_slot_params)
 
     @time_slots = TimeSlot.for_school(@school).for_day(@date)
     
--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -49,17 +49,17 @@ class TimeSlot < ApplicationRecord
   }
   default_scope { order('start ASC') }
 
-  def self.create_week_daily(school_year:, params:)
-    period_start = school_year.start
-    period_end = school_year.end
-    start = Time.parse(params[:start])
-    finish = Time.parse(params[:end])
-    title = params[:title]
-    time_slots = []
+  def self.create_week_daily(school_year:, date:, params:)
+    period_start = date.to_datetime.beginning_of_day
+    period_end   = school_year.end
+    start        = Time.parse(params[:start])
+    finish       = Time.parse(params[:end])
+    title        = params[:title]
+    time_slots   = []
     (period_start.to_i..period_end.to_i).step(1.day) do |day|
       day = Time.zone.at(day).to_date
-      s = Utils.datetime_from_date_and_time(day, start)
-      e = Utils.datetime_from_date_and_time(day, finish)
+      s   = Utils.datetime_from_date_and_time(day, start)
+      e   = Utils.datetime_from_date_and_time(day, finish)
       time_slots << { start: s, end: e, school_year_id: school_year.id, title: title }
     end
     TimeSlot.import(time_slots)
@@ -67,9 +67,9 @@ class TimeSlot < ApplicationRecord
 
   def self.update_with_type(time_slot:, type:, params:)
     s, e = params[:start], params[:end]
-    day = time_slot.start.to_date
-    s = Utils.datetime_from_date_and_time(day, Time.zone.parse(s))
-    e = Utils.datetime_from_date_and_time(day, Time.zone.parse(e))
+    day  = time_slot.start.to_date
+    s    = Utils.datetime_from_date_and_time(day, Time.zone.parse(s))
+    e    = Utils.datetime_from_date_and_time(day, Time.zone.parse(e))
     case type.to_sym
     when :one
       time_slot.update(params)
