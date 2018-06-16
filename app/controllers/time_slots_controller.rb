8e1b6e658b6e5f1c998bd95322febc870dcd544e
kristiyan <kristiyan.c@gmail.com>
2018-06-16 20:00:03 +0300

    Organise time slot action

--- app/controllers/time_slots_controller.rb
+++ app/controllers/time_slots_controller.rb
@@ -6,7 +6,7 @@ class TimeSlotsController < ApplicationController
 
     respond_to do |format|
       format.html { }
-      format.js { render action: "refresh_card"}
+      format.js { }
     end
   end
 
@@ -15,39 +15,32 @@ class TimeSlotsController < ApplicationController
     @date       = (params[:date] && Date.parse(params[:date])) || Date.today
     school_year = SchoolYear.find(time_slot_params[:school_year_id])
 
-    TimeSlot.create_week_daily(school_year: school_year, date: @date, params: time_slot_params)
-
-    @time_slots = TimeSlot.for_school(@school).for_day(@date)
+    @slots = TimeSlot.create_week_daily(school_year: school_year, date: @date, params: time_slot_params)
+    @slot = TimeSlot.find(@slots.ids.first)
     
     respond_to do |format|
-      format.js { render action: "refresh_card"}
+      format.js { }
     end
   end
 
   def update
-    time_slot = TimeSlot.find(params[:id])
-    #Type is one of [:one, :all]
-    TimeSlot.update_with_type(time_slot: time_slot, type: type, params: update_slot_params)
-    @school = time_slot.school
-    @date = time_slot.start.to_date
-    @time_slots = TimeSlot.for_school(@school).for_day(@date)
+    @slot = TimeSlot.find(params[:id])
+    TimeSlot.update_with_type(time_slot: @slot, type: params[:type], params: update_slot_params)
+    @date = @slot.start.to_date
 
     respond_to do |format|
-      format.js { render action: "refresh_card"}
+      format.js { }
     end
   end
 
   def destroy
-    time_slot = TimeSlot.find(params[:id])
-    @school = time_slot.school
-    @date = time_slot.start.to_date
-
-    #Type is one of [:one, :all]
-    TimeSlot.destroy_with_type(time_slot: time_slot, type: type)
-    @time_slots = TimeSlot.for_school(@school).for_day(@date)
+    @slot = TimeSlot.find(params[:id])
+    @school = @slot.school
+    @date = @slot.start.to_date
+    TimeSlot.destroy_with_type(time_slot: @slot, type: params[:type])
 
     respond_to do |format|
-      format.js { render action: "refresh_card"}
+      format.js { }
     end
   end
 
