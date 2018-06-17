c3ea406ec3fc0efee7dd26c39e4a6af513a71429
kristiyan <kristiyan.c@gmail.com>
2018-06-17 12:34:05 +0300

    Organised student invitations

--- app/controllers/schools/student_invitations_controller.rb
+++ app/controllers/schools/student_invitations_controller.rb
@@ -1,21 +1,20 @@
 class Schools::StudentInvitationsController < ApplicationController
+  before_action :set_params
+
   def index
-    @school = School.find(params[:school_id])
   end
 
   def create
-    school = School.find(params[:school_id])
-    group = Group.find(params[:group_id])
     role = Role.find_by(name: "Student")
-    @is_invited = Assignment.invite(role: role, school: school, user: user_params.merge(group_id: group.id))
+    @is_invited = Assignment.invite(role: role, school: @school, user: user_params.merge(group_id: @group.id))
 
     respond_to do |format|
       if @is_invited
         @user = User.find_by(email: user_params[:email])
-        format.html { redirect_to school_group_path(school, group), notice: "Invited!" }
+        format.html { redirect_to school_group_path(@school, @group), notice: "Invited!" }
         format.js {  }
       else
-        format.html {redirect_to school_group_path(school, group), alert: "Failed to invite!"}
+        format.html {redirect_to school_group_path(@school, @group), alert: "Failed to invite!"}
         format.js {  }
       end
     end
@@ -25,5 +24,10 @@ class Schools::StudentInvitationsController < ApplicationController
 
   def user_params
     params.permit(:email, :first_name, :last_name, :number_in_class)
+  end
+
+  def set_params
+    @school = School.find(params[:school_id])
+    @group = Group.find(params[:group_id])
   end
 end
