86a904b0c0296f62ef16d7afd455888d6bc4b0d4
kristiyan <kristiyan.c@gmail.com>
2018-06-19 15:50:07 +0300

    Show 'invite parent' only to student, parent of headteacher

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -4,6 +4,12 @@ class UserPolicy < ApplicationPolicy
     true
   end
 
+  def invite_parent?
+    user == record ||
+      user.parent_of?(record) ||
+      user.headteacher?(record.group)
+  end
+
   def schedule?
     user == record
   end
