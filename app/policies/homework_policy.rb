0b55110a9a5a0d1fa8cae2c96f74b060b289b2b1
kristiyan <kristiyan.c@gmail.com>
2018-06-19 14:53:29 +0300

    Homeworks policy

--- app/policies/homework_policy.rb
+++ app/policies/homework_policy.rb
@@ -1,3 +1,20 @@
 class HomeworkPolicy < ApplicationPolicy
 
+  class Scope < Scope
+    def resolve
+      scope.joins(course: [school: [:assignments]]).where(assignments: {user_id: user.id})
+    end
+  end
+
+  def create?
+    user.teaching_course?(record.course)
+  end
+
+  def update?
+    user.teaching_course?(record.course) or user.headteacher?(record.group)
+  end
+
+  def destroy?
+    user.teaching_course?(record.course) or user.headteacher?(record.group)
+  end
 end
