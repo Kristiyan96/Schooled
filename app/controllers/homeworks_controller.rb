b0cd41cbffe021b903224fe377ccbc2b1c6f05cd
kristiyan <kristiyan.c@gmail.com>
2018-06-19 12:07:35 +0300

    Final fixes

--- app/controllers/homeworks_controller.rb
+++ app/controllers/homeworks_controller.rb
@@ -33,6 +33,7 @@ class HomeworksController < ApplicationController
     @homeworks        = @group.homeworks
     @homeworks_past   = @group.homeworks.where('deadline < ?', DateTime.now)
     @homeworks_future = @group.homeworks.where('deadline > ?', DateTime.now)
+    @homeworks = @group.homeworks.where(course: @course).order(:created_at)
 
     respond_to do |format|
       if @homework.update(homework_params)
@@ -51,6 +52,7 @@ class HomeworksController < ApplicationController
     @homework.destroy
     @homeworks_past   = @group.homeworks.where('deadline < ?', DateTime.now)
     @homeworks_future = @group.homeworks.where('deadline > ?', DateTime.now)
+    @homeworks = @group.homeworks.where(course: @course).order(:created_at)
     
     respond_to do |format|
       format.html { redirect_to homeworks_url, notice: 'Homework was successfully destroyed.' }
