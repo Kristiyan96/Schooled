46d5ed6466ceae32921cb8ab64fa78791b93073b
kristiyan <kristiyan.c@gmail.com>
2018-06-19 13:46:51 +0300

    Schedule policy

--- app/policies/course_policy.rb
+++ app/policies/course_policy.rb
@@ -1,3 +1,31 @@
 class CoursePolicy < ApplicationPolicy
 
+  class Scope < Scope
+    def resolve
+      scope.joins(school: [:assignments]).where(assignments: {user_id: user.id, role_id: 3})
+    end
+  end
+  
+  def show? 
+    user.admin? ||
+      user.headmaster?(record.school) ||
+      user.teaching_course?(record)
+  end
+
+  def create?
+    user.admin? ||
+      user.headmaster?(record.school) ||
+      user.headteacher?(record.group)
+  end
+
+  def update?
+    user.admin? ||
+      user.headmaster?(record.school) ||
+      user.teaching_course?(record)
+  end
+
+  def destroy?
+    user.admin? ||
+      user.headmaster?(record.school)
+  end
 end
