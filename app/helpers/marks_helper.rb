ee61a7587def26fd934909d99cdc22ce20eea6f5
kristiyan <kristiyan.c@gmail.com>
2018-06-23 17:00:09 +0300

    Showing marks progress on students profile page

--- app/helpers/marks_helper.rb
+++ app/helpers/marks_helper.rb
@@ -17,4 +17,18 @@ module MarksHelper
   def formatted_mark(mark)
     mark.grade == mark.grade.floor ? '%g' % mark.grade : ('%.2f' % mark.grade)
   end
+
+  def average_mark_until_now_weekly_this_semester(student)
+    periods = weeks_until_now_this_semester(student.group.school)
+    start = periods.first.beginning_of_day
+    averages = []
+
+    periods.each do |p|
+      averages << (Mark
+        .where('student_id=? AND created_at>? AND created_at<?', student.id, start, p.end_of_day)
+        .average(:grade) || 0)
+    end
+
+    averages
+  end
 end
