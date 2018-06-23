0d1802ec759ce4f6cf3e764a8567b6731cb5528e
kristiyan <kristiyan.c@gmail.com>
2018-06-23 11:52:06 +0300

    Style student profile's marks table

--- app/helpers/marks_helper.rb
+++ app/helpers/marks_helper.rb
@@ -1,4 +1,19 @@
 module MarksHelper
+
+  def mark_color(mark)
+    if mark.grade<2.5
+      "danger"
+    elsif mark.grade<3.5
+      "warning"
+    elsif mark.grade<4.5
+      "primary"
+    elsif mark.grade<5.5
+      "info"
+    else
+      "success" 
+    end
+  end
+  
   def formatted_mark(mark)
     mark.grade == mark.grade.floor ? '%g' % mark.grade : ('%.2f' % mark.grade)
   end
