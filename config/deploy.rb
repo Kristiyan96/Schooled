fb45ee7e76014553833f818d40a53ba9250e4128
Nikola Jichev <njichev@gmail.com>
2018-07-01 15:42:40 +0300

    Upload master.key for rails

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,6 +37,7 @@ set :puma_init_active_record, true # Change to true if using ActiveRecord
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
 
+set :linked_files, %w{config/master.key}
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
