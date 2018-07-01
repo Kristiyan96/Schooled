e9d64f5cab4d40c690313683f0c54e7b5758f605
Nikola Jichev <njichev@gmail.com>
2018-07-01 17:22:41 +0300

    Revert adding config/database.yml file.

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,7 +37,7 @@ set :puma_init_active_record, true # Change to true if using ActiveRecord
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
 
-set :linked_files, %w[config/master.key config/database.yml]
+set :linked_files, %w[config/master.key]
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
